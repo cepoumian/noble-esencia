a1c438929e0bab8f8f56c282d4f5f9ff5dc3c844
cepoumian <cepoumian@gmail.com>
2025-05-26 10:31:19 -0600

    Add text reveal to hero and create scroll text section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       className={`${raleway.variable} ${gambarino.variable} antialiased`}
     >
       <body className="bg-neutral-900 text-white">
-        <main>
+        <main className="pt-14 md:pt-16">
           <NavBar />
           {children}
         </main>
--- src/components/RevealText.tsx
+++ src/components/RevealText.tsx
@@ -77,6 +77,7 @@ export const RevealText = ({
         >
           <span className="reveal-text-word mt-0 inline-block translate-y-[120%] will-change-transform">
             {word}
+            {index < words.length - 1 && <>&nbsp;</>}
           </span>
         </span>
       ))}

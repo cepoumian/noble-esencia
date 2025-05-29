f9410d9cb4a5ee59697ddc88033184051169c155
cepoumian <cepoumian@gmail.com>
2025-05-29 12:55:03 -0600

    Create individual fragrance page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,10 +33,8 @@ export default function RootLayout({
       className={`${raleway.variable} ${gambarino.variable} antialiased`}
     >
       <body className="bg-neutral-900 text-white">
-        <main className="pt-14 md:pt-16">
-          <NavBar />
-          {children}
-        </main>
+        <NavBar />
+        <main className="pt-14 md:pt-16">{children}</main>
 
         <Footer />
       </body>
